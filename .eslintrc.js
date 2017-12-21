1d3765d5d1659e68a30d28898c638483cdc86fd5
Jonas <602356374@qq.com>
2017-12-21 18:01:59 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     // allow async-await
+    "semi":'off',
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
