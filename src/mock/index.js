aab39f4677da59386cedb4d82ec2953eb211f67e
Jonas <602356374@qq.com>
2018-01-03 11:32:57 +0800

    mockjs

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,14 +1,19 @@
 var Mock = require('mockjs')
 Mock.mock(/\/login/, 'post', {
-  'token': 'admin',
+  'token': 'admin1',
   'age|1-100': 100
 })
 
-Mock.mock(/\/getUserinfo/, 'get', {
-  'user': {
-    'name': 'liwei',
-    'phone': '17612151860'
-  },
-  'role': ['admin']
+Mock.mock(/\/getUserinfo/, 'get', function (config) {
+  console.log(config)
+  if (config.url === '/getUserinfo?0=admin') {
+    return {
+      'user': {
+        'name': 'liwei',
+        'phone': '17612151860'
+      },
+      'role': ['admin']
+    }
+  }
 })
 export default Mock
