ffab8ef26adf3830f9f608659808b7040b6de390
Jonas <602356374@qq.com>
2018-01-04 14:13:38 +0800

    布局样式

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,20 @@ export default {
 </script>
 
 <style>
+html,body,#app{
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+ul {
+  list-style-type: none;
 }
 </style>
