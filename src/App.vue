2ab170e9df7d5837c14e72ef8c2e45a685363af1
liweili50 <liweili50@163.com>
2018-07-13 16:12:45 +0800

    布局调整

--- src/App.vue
+++ src/App.vue
@@ -12,19 +12,29 @@ export default {
 
 <style>
 html,body,#app{
+  width: 100%;
+  height: 100%;
   padding: 0;
   margin: 0;
-  height: 100%;
-  width: 100%;
+  box-sizing: border-box;
+  background-color: #F8F8F8;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 ul {
   list-style-type: none;
+}
+.el-dialog--center .el-dialog__body{
+   padding: 0px;
+}
+.el-dialog__header{
+  text-align: left;
+}
+.el-dialog__footer {
+  text-align: center;
 }
 </style>
