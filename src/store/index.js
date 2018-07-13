70976c1ab8b34d231d00db057f899febcd89ae61
liweili50 <liweili50@163.com>
2018-07-13 16:16:36 +0800

    新增tab栏

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import permission from './modules/permission'
 import user from './modules/user'
+import tab from './modules/tab'
 import getters from './getters'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     user,
-    permission
+    permission,
+    tab
   },
   getters
 })
