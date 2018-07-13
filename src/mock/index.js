b280bc5f9d38336f645bd312284187e987b13e17
liweili50 <liweili50@163.com>
2018-07-13 16:14:52 +0800

    模拟数据

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,18 +1,17 @@
 var Mock = require('mockjs')
 Mock.mock(/\/login/, 'post', {
+  'code': 0,
   'token': 'admin'
 })
 
 Mock.mock(/\/getUserinfo/, 'get', function (config) {
-  console.log(config)
-  if (config.url === '/getUserinfo') {
-    return {
-      'user': {
-        'name': 'liwei',
-        'phone': '17612151860'
-      },
-      'role': ['admin']
-    }
+  return {
+    'user': {
+      'name': 'Jonas',
+      'phone': '17612151860'
+    },
+    'rolesList': ['admin'],
+    'code': 0
   }
 })
 export default Mock
